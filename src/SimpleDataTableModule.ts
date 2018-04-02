cbabe12ec396da7bf596582862266079fee1e23f
Sebastian Foth <Master_rg@t-online.de>
2018-04-02 21:27:38 +0200

    Updated SimpleDataTableModule Added reference to SimpleDataTableDirective

--- src/SimpleDataTableModule.ts
+++ src/SimpleDataTableModule.ts
@@ -1,13 +1,16 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {SimpleDataTableDirective} from "./directives/simpledatatable.directive";
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
+        SimpleDataTableDirective
     ],
     exports: [
+        SimpleDataTableDirective
     ]
 })
 export class SimpleDataTableModule {
